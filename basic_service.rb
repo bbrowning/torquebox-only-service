e3932df54482cbb81ccbd87a31eee3811f8857b0
Ben Browning <bbrownin@redhat.com>
2011-02-02 08:33:04 -0500

    Sample app for deploying (almost) headless TorqueBox Services

---
+++ basic_service.rb
@@ -0,0 +1,13 @@
+class BasicService
+  def initialize(options={})
+    puts "BasicService Initializing"
+  end
+
+  def start
+    puts "BasicService Starting"
+  end
+
+  def stop
+    puts "BasicService Stopping"
+  end
+end
